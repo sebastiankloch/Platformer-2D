8139f68697617b866fd80992a7bb2bdc73136293
sebastiankloch <32013945+sebastiankloch@users.noreply.github.com>
2017-09-23 20:19:50 +0200

    rm unnecessary characters

--- Example scripts/AtlasGenerator.cs
+++ Example scripts/AtlasGenerator.cs
@@ -1,4 +1,4 @@
-#if UNITY_EDITOR
+#if UNITYEDITOR
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,40 +6,40 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class AtlasGenerator : MonoBehaviour {
 
-    public bool _createAtlas;
-    public bool _createdAtlas;
-    public string _atlasName = "atlas";
-    public string _path = "Assets/_Sprites/Reflections/";
-    public Texture2D[] _textures;
+    public bool createAtlas;
+    public bool createdAtlas;
+    public string atlasName = "atlas";
+    public string path = "Assets/Sprites/Reflections/";
+    public Texture2D[] textures;
     void Update () {
-        if (_createAtlas && !_createdAtlas)
+        if (createAtlas && !createdAtlas)
         {
-            _CreateAtlas();
-            _createAtlas = false;
-            _createdAtlas = true;
+            CreateAtlas();
+            createAtlas = false;
+            createdAtlas = true;
         }
 	}
 
-    void _CreateAtlas()
+    void CreateAtlas()
     {
-        if (!_createAtlas) return;
-        int __widthOfTextures = _textures[0].width;
-        int __maxColumn = Mathf.CeilToInt(Mathf.Sqrt((float)_textures.GetLength(0)));
-        int __column = 0;
-        int __row = 0;
+        if (!createAtlas) return;
+        int widthOfTextures = textures[0].width;
+        int maxColumn = Mathf.CeilToInt(Mathf.Sqrt((float)textures.GetLength(0)));
+        int column = 0;
+        int row = 0;
 
-        Texture2D __atlas = new Texture2D(__maxColumn * __widthOfTextures, Mathf.CeilToInt(_textures.GetLength(0) / __maxColumn) * __widthOfTextures + __widthOfTextures, TextureFormat.RGBA32, false);
-        print("__atlas.width: " + __atlas.width + " __atlas.height: " + __atlas.height);
-        for (int id = 0; id < _textures.GetLength(0); ++id)
+        Texture2D atlas = new Texture2D(maxColumn * widthOfTextures, Mathf.CeilToInt(textures.GetLength(0) / maxColumn) * widthOfTextures + widthOfTextures, TextureFormat.RGBA32, false);
+        print("atlas.width: " + atlas.width + " atlas.height: " + atlas.height);
+        for (int id = 0; id < textures.GetLength(0); ++id)
         {
-            __atlas.SetPixels(__column * __widthOfTextures, __row * __widthOfTextures, __widthOfTextures, __widthOfTextures, _textures[id].GetPixels());
-            ++__column;
-            if (__column > __maxColumn - 1) { __column = 0; ++__row; }
+            atlas.SetPixels(column * widthOfTextures, row * widthOfTextures, widthOfTextures, widthOfTextures, textures[id].GetPixels());
+            ++column;
+            if (column > maxColumn - 1) { column = 0; ++row; }
             
         }
-        __atlas.Apply();
-        System.IO.File.WriteAllBytes(_path + _atlasName + ".png", __atlas.EncodeToPNG());
-        Debug.Log("<color=green><b>SUCCESSFULY</b></color> created: " + _atlasName);
+        atlas.Apply();
+        System.IO.File.WriteAllBytes(path + atlasName + ".png", atlas.EncodeToPNG());
+        Debug.Log("<color=green><b>SUCCESSFULY</b></color> created: " + atlasName);
     }
 }
 #endif
